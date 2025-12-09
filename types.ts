eba49b0916fdd3d8f16cb6d78b52d3c66e768ce3
Erik R <erik.riklund@outlook.com>
2025-12-09 20:01:43 +0100

    add documentation

--- types.ts
+++ types.ts
@@ -1,16 +1,6 @@
-// RFC 6901 -> https://datatracker.ietf.org/doc/html/rfc6901
-// RFC 6902 -> https://datatracker.ietf.org/doc/html/rfc6902
-
 /**
- * Defines the common structure for all JSON Patch operations (RFC 6902).
- * The 'op' field specifies the type of operation, and 'path' indicates the target location.
- */
-type Operation<
-  T extends "add" | "remove" | "replace" | "move" | "copy" | "test",
-> = { op: T; path: `/${string}` };
-
-/**
- * ?
+ * Represents a single JSON Patch operation
+ * (see {@link https://datatracker.ietf.org/doc/html/rfc6902#section-4|RFC 6902}).
  */
 export type Patch =
   | AddOperation
@@ -21,62 +11,162 @@ export type Patch =
   | TestOperation;
 
 /**
- * The `add` operation performs one of the following functions,
+ * Defines the common structure for all JSON Patch operations (RFC 6902).
+ * The 'op' field specifies the type of operation, and 'path' indicates the target location.
+ */
+type Operation<
+  T extends "add" | "remove" | "replace" | "move" | "copy" | "test",
+> = { op: T; path: `/${string}` };
+
+/**
+ * The "add" operation performs one of the following functions,
  * depending upon what the target location references:
  *
- * - If the target location specifies an array index, a new value is inserted into the array at the specified index.
- * - If the target location specifies an object member that does not already exist, a new member is added to the object.
- * - If the target location specifies an object member that does exist, that member's value is replaced.
+ * - If the target location specifies an array index,
+ *   a new value is inserted into the array at the specified index.
  *
- * The operation object MUST contain a "value" member whose content specifies the value to be added.
+ * - If the target location specifies an object member that does
+ *   not already exist, a new member is added to the object.
  *
- * For example:
+ * - If the target location specifies an object member that does exist,
+ *   that member's value is replaced.
+ *
+ * The operation object MUST contain a "value" member whose content
+ * specifies the value to be added.
  *
  * `{ "op": "add", "path": "/a/b/c", "value": [ "foo", "bar" ] }`
  *
  * When the operation is applied, the target location MUST reference one of:
  *
- * - The root of the target document - whereupon the specified value becomes the entire content of the target document.
- * - A member to add to an existing object - whereupon the supplied value is added to that object at the indicated location.
- *   If the member already exists, it is replaced by the specified value.
- * - An element to add to an existing array - whereupon the supplied value is added to the array at the indicated location.
- *   Any elements at or above the specified index are shifted one position to the right.  The specified index MUST NOT be greater
- *   than the number of elements in the array.  If the "-" character is used to index the end of the array (see RFC 6901),
+ * - The root of the target document - whereupon the specified value
+ *   becomes the entire content of the target document.
+ *
+ * - A member to add to an existing object - whereupon the supplied
+ *   value is added to that object at the indicated location.
+ *
+ *   _If the member already exists, it is replaced by the specified value._
+ *
+ * - An element to add to an existing array - whereupon the supplied
+ *   value is added to the array at the indicated location. Any elements
+ *   at or above the specified index are shifted one position to the right.
+ *
+ *   The specified index MUST NOT be greater than the number of elements in the array.
+ *
+ *   If the "-" character is used to index the end of the array (see RFC 6901),
  *   this has the effect of appending the value to the array.
  *
- * Because this operation is designed to add to existing objects and arrays, its target location will often not exist.
- * Although the pointer's error handling algorithm will thus be invoked, this specification defines the error handling behavior
- * for "add" pointers to ignore that error and add the value as specified.
+ * Because this operation is designed to add to existing objects and arrays, its target
+ * location will often not exist. Although the pointer's error handling algorithm will
+ * thus be invoked, this specification defines the error handling behavior for "add" pointers
+ * to ignore that error and add the value as specified.
  *
- * However, the object itself or an array containing it does need to exist, and it remains an error for that not to be the case.
- * For example, an "add" with a target location of "/a/b" in the document
- * `{ "a": { "foo": 1 } }` is not an error, because "a" exists, and "b" will be added to its value.
+ * However, the object itself or an array containing it does need to exist,
+ * and it remains an error for that not to be the case. For example, an "add"
+ * with a target location of "/a/b" in the document `{ "a": { "foo": 1 } }`
+ * is not an error, because "a" exists, and "b" will be added to its value.
  *
  * It is an error in the document `{ "q": { "bar": 2 } }` because "a" does not exist.
  */
 export type AddOperation = Operation<"add"> & { value: unknown };
 
 /**
- * ?
+ * The `remove` operation removes the value at the target location.
+ *
+ * `{ "op": "remove", "path": "/a/b/c" }`
+ *
+ * The target location MUST exist for the operation to be successful.
+ *
+ * If removing an element from an array, any elements above the specified
+ * index are shifted one position to the left.
  */
 export type RemoveOperation = Operation<"remove">;
 
 /**
- * ?
+ * The "replace" operation replaces the value at the target location
+ * with a new value. The operation object MUST contain a "value" member
+ * whose content specifies the replacement value.
+ *
+ * `{ "op": "replace", "path": "/a/b/c", "value": "foo" }`
+ *
+ * The target location MUST exist for the operation to be successful.
+ *
+ * This operation is functionally identical to a "remove" operation for a value,
+ * followed immediately by an "add" operation at the same location with the replacement value.
  */
 export type ReplaceOperation = Operation<"replace"> & { value: unknown };
 
 /**
- * ?
+ * The "move" operation removes the value at a specified location and
+ * adds it to the target location.
+ *
+ * `{ "op": "move", "from": "/a/b/c", "path": "/a/b/d" }`
+ *
+ * The operation object MUST contain a "from" member, which is a string
+ * containing a JSON Pointer value that references the location in the
+ * target document to move the value from.
+ *
+ * The "from" location MUST exist for the operation to be successful.
+ *
+ * This operation is functionally identical to a "remove" operation on
+ * the "from" location, followed immediately by an "add" operation at
+ * the target location with the value that was just removed.
+ *
+ * The "from" location MUST NOT be a proper prefix of the "path" location;
+ * i.e., a location cannot be moved into one of its children.
  */
 export type MoveOperation = Operation<"move"> & { from: `/${string}` };
 
 /**
- * ?
+ * The "copy" operation copies the value at a specified location to the
+ * target location.
+ *
+ * `{ "op": "copy", "from": "/a/b/c", "path": "/a/b/e" }`
+ *
+ * The operation object MUST contain a "from" member, which is a string
+ * containing a JSON Pointer value that references the location in the
+ * target document to copy the value from.
+ *
+ * The "from" location MUST exist for the operation to be successful.
+ *
+ * This operation is functionally identical to an "add" operation at the
+ * target location using the value specified in the "from" member.
  */
 export type CopyOperation = Operation<"copy"> & { from: `/${string}` };
 
 /**
- * ?
+ * The "test" operation tests that a value at the target location is
+ * equal to a specified value.
+ *
+ * `{ "op": "test", "path": "/a/b/c", "value": "foo" }`
+ *
+ * The operation object MUST contain a "value" member that conveys the
+ * value to be compared to the target location's value.
+ *
+ * The target location MUST be equal to the "value" value for the
+ * operation to be considered successful.
+ *
+ * Here, "equal" means that the value at the target location and the
+ * value conveyed by "value" are of the same JSON type, and that they
+ * are considered equal by the following rules for that type:
+ *
+ * - strings are considered equal if they contain the same number of
+ *   Unicode characters and their code points are byte-by-byte equal.
+ *
+ * - numbers are considered equal if their values are numerically equal.
+ *
+ * - arrays are considered equal if they contain the same number of values,
+ *   and if each value can be considered equal to the value at the corresponding
+ *   position in the other array, using this list of type-specific rules.
+ *
+ * - objects are considered equal if they contain the same number of members,
+ *   and if each member can be considered equal to a member in the other object,
+ *   by comparing their keys (as strings) and their values (using this list of type-specific rules).
+ *
+ * - literals (false, true, and null): are considered equal if they are the same.
+ *
+ * Note that the comparison that is done is a logical comparison;
+ * e.g., whitespace between the member values of an array is not significant.
+ *
+ * Also, note that ordering of the serialization of object members is not significant.
  */
 export type TestOperation = Operation<"test"> & { value: unknown };
